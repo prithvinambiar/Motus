6253265f97674cd7d1fab5824d4339025a49b783
prithvinambiar <prithvinambiar@gmail.com>
2017-02-19 23:31:11 +0530

    Implemented SentimentAnalyser Added support for junit test

--- src/main/java/com/motus/SentimentAnalyser.java
+++ src/main/java/com/motus/SentimentAnalyser.java
@@ -1,7 +1,14 @@
 package com.motus;
 
+import edu.stanford.nlp.simple.Sentence;
+import edu.stanford.nlp.simple.SentimentClass;
+
 class SentimentAnalyser
 {
+    SentimentClass analyse(String text){
+        Sentence sentence = new Sentence(text);
+        return sentence.sentiment();
+    }
     public static void main(String[] args) {
         System.out.println("hello");
     }
