aa427e084d280b2732c8b7325b2bae84ef867900
prithvinambiar <prithvinambiar@gmail.com>
2017-02-28 23:53:07 +0530

    Implemented twitter search api

--- src/main/java/com/motus/data/twitter/TwitterSearchApi.java
+++ src/main/java/com/motus/data/twitter/TwitterSearchApi.java
@@ -1,20 +1,35 @@
 package com.motus.data.twitter;
 
-import twitter4j.Twitter;
+import twitter4j.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class TwitterSearchApi implements TwitterApi{
 
     private final Twitter twitter;
 
-    public TwitterSearchApi(Twitter twitter) {
+    TwitterSearchApi(Twitter twitter) {
 
         this.twitter = twitter;
     }
 
     @Override
     public List<String> GetTweets(String searchString) {
-        return null;
+        Query query = new Query();
+        query.setQuery(searchString);
+        query.resultType(Query.ResultType.recent);
+        try {
+            QueryResult search = twitter.search(query);
+            List<Status> statuses = search.getTweets();
+            List<String> tweets = new ArrayList<>();
+            for (Status status : statuses) {
+                tweets.add(status.getText());
+            }
+            return tweets;
+        } catch (TwitterException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- src/main/test/com/motus/data/twitter/TwitterSearchApiTest.java
+++ src/main/test/com/motus/data/twitter/TwitterSearchApiTest.java
@@ -1,30 +1,33 @@
 package com.motus.data.twitter;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import twitter4j.*;
 
-import java.util.List;
+import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class TwitterSearchApiTest {
+
     @Test
     public void should_get_tweets_for_given_search_string_using_search_api() throws TwitterException {
-        Query query = new Query();
-        query.setQuery("prithvi");
-        query.resultType(Query.ResultType.recent);
-        Twitter twitter = new TwitterFactory().getInstance();
-        QueryResult search = twitter.search(query);
 
-        Twitter mock = mock(Twitter.class);
+        Twitter twitter = mock(Twitter.class);
+        QueryResult queryResult = mock(QueryResult.class);
+        when(twitter.search(any(Query.class))).thenReturn(queryResult);
 
-        TwitterApi twitterApi = new TwitterSearchApi(mock);
-        List<String> tweets = twitterApi.GetTweets("linux");
+        TwitterApi twitterApi = new TwitterSearchApi(twitter);
+        twitterApi.GetTweets("linux");
 
-        verify(mock).search(isA(Query.class));
+        ArgumentCaptor<Query> queryArgumentCaptor = ArgumentCaptor.forClass(Query.class);
 
+        verify(twitter).search(queryArgumentCaptor.capture());
+        assertEquals("linux", queryArgumentCaptor.getValue().getQuery());
     }
 }
